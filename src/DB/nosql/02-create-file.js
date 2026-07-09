20bb13fe7bd4ebfd3d9a253997d56d9f1eb3ffe9
History Tracers <historytracers@gmail.com>
2026-07-09 18:13:39 +0000

    database: File table

---
+++ src/DB/nosql/02-create-file.js
@@ -0,0 +1,22 @@
+db.createCollection("file", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["fil_id", "fil_desc"],
+      properties: {
+        _id: {
+          bsonType: "objectId",
+          description: "MongoDB internal identifier"
+        },
+        fil_id: {
+          bsonType: "binData",
+          description: "Unique binary identifier for the file"
+        },
+        fil_desc: {
+          bsonType: "string",
+          description: "Description of the file"
+        }
+      }
+    }
+  }
+});
---
+++ src/DB/sql/03-create-file.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS file (
+    fil_id          BINARY(16)    NOT NULL PRIMARY KEY,
+    fil_desc        TEXT          NOT NULL
+);
