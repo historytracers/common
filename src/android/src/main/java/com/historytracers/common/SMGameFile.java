80237ba3d67356dd3f7d131dd9ce7ad18dffb41d
History Tracers <historytracers@gmail.com>
2026-07-31 03:09:14 +0000

    fields: Add title to Game

--- src/android/src/main/java/com/historytracers/common/SMGameFile.java
+++ src/android/src/main/java/com/historytracers/common/SMGameFile.java
@@ -7,6 +7,7 @@ import com.google.gson.annotations.SerializedName;
 import java.util.List;
 
 public class SMGameFile {
+    public String title;
     public List<String> sources;
     public List<String> license;
 
