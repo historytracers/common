10b822b29807fc8351fb90052f11d6bab5d43ba6
History Tracers <historytracers@gmail.com>
2026-08-14 15:10:07 +0000

    Add creator

--- src/go/creator.go
+++ src/go/creator.go
@@ -5,15 +5,37 @@ package common
 // HTAddNewClassToIdx appends a new class content value to the last content
 // entry of the class index and updates the timestamp.
 func HTAddNewClassToIdx(index *ClassIdx, newFile string) {
+	HTAddNewClassToIdxAfter(index, newFile, "")
+}
+
+// HTAddNewClassToIdxAfter inserts a new class content value into the value
+// vector right after the entry with the given ID. If afterID is empty or is
+// not found in any content entry, the value is appended to the last content
+// entry. The timestamp is updated.
+func HTAddNewClassToIdxAfter(index *ClassIdx, newFile string, afterID string) {
+	newValue := ClassContentValue{ID: newFile, Name: "", Desc: ""}
+
+	if afterID != "" {
+		for i := range index.Content {
+			content := &index.Content[i]
+			for j := range content.Value {
+				if content.Value[j].ID == afterID {
+					content.Value = append(content.Value, ClassContentValue{})
+					copy(content.Value[j+2:], content.Value[j+1:])
+					content.Value[j+1] = newValue
+					index.LastUpdate[0] = HTUpdateTimestamp()
+					return
+				}
+			}
+		}
+	}
+
 	lastContent := len(index.Content) - 1
 	if lastContent < 0 {
 		return
 	}
 
 	content := &index.Content[lastContent]
-
-	newValue := ClassContentValue{ID: newFile, Name: "", Desc: ""}
-
 	content.Value = append(content.Value, newValue)
 
 	index.LastUpdate[0] = HTUpdateTimestamp()
@@ -49,15 +71,37 @@ func HTNewFamilySetDefaultValues(family *Family, lang string, fileName string) {
 // HTAddNewFamilyToIdx appends a new family content value to the last content
 // entry of the family index and updates the timestamp.
 func HTAddNewFamilyToIdx(index *IdxFamily, newFile string, lang string) {
+	HTAddNewFamilyToIdxAfter(index, newFile, lang, "")
+}
+
+// HTAddNewFamilyToIdxAfter inserts a new family content value into the value
+// vector right after the entry with the given ID. If afterID is empty or is
+// not found in any content entry, the value is appended to the last content
+// entry. The timestamp is updated.
+func HTAddNewFamilyToIdxAfter(index *IdxFamily, newFile string, lang string, afterID string) {
+	newValue := IdxFamilyValue{ID: newFile, GEDCOM: "gedcom/" + newFile + "_" + lang + ".ged", CSV: "csv/" + newFile + "_" + lang + ".csv"}
+
+	if afterID != "" {
+		for i := range index.Contents {
+			content := &index.Contents[i]
+			for j := range content.Value {
+				if content.Value[j].ID == afterID {
+					content.Value = append(content.Value, IdxFamilyValue{})
+					copy(content.Value[j+2:], content.Value[j+1:])
+					content.Value[j+1] = newValue
+					index.LastUpdate[0] = HTUpdateTimestamp()
+					return
+				}
+			}
+		}
+	}
+
 	lastContent := len(index.Contents) - 1
 	if lastContent < 0 {
 		return
 	}
 
 	content := &index.Contents[lastContent]
-
-	newValue := IdxFamilyValue{ID: newFile, GEDCOM: "gedcom/" + newFile + "_" + lang + ".ged", CSV: "csv/" + newFile + "_" + lang + ".csv"}
-
 	content.Value = append(content.Value, newValue)
 
 	index.LastUpdate[0] = HTUpdateTimestamp()
